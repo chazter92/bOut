8520b848474dd581bca9442d0b77daeb30f9bf73
Mario14264 <mariorodas92@gmail.com>
2014-11-03 16:49:51 -0600

    Top
    Para el top5 general y por categorías ;)

---
+++ top.php
@@ -0,0 +1,43 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>B-Out! | About</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!--  jquery plguin -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+</head>
+<body>
+	<?php include_once('header.php');?>
+</div>
+</div>
+<!-- start slider -->
+<div class="slider_bg">
+<div class="wrap">
+	<div class="slider">
+		<h2>Bout!</h2>
+		<h3>Mientras exista diversion, lo seguiremos haciendo!</h3>
+	</div>
+</div>
+</div>
+<!-- start main -->
+<div class="main_bg">
+<div class="wrap">
+	<div class="main">
+		<div class="content" >
+			
+		</div>
+	</div>
+</div>
+</div>
+	<?php include_once('footer.php');?>
+</body>
+</html>
