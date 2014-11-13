d7e805b80645b989a03fef3310143d6ca61d4be9
Mario14264 <mariorodas92@gmail.com>
2014-11-12 23:35:19 -0600

    Intento
    Intento para cargar las categorías

--- top.php
+++ top.php
@@ -14,6 +14,70 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 <!--  jquery plguin -->
 <script type="text/javascript" src="js/jquery.min.js"></script>
+<!-- start gallery -->
+	<script type="text/javascript" src="js/jquery.easing.min.js"></script>	
+	<script type="text/javascript" src="js/jquery.mixitup.min.js"></script>
+	<script type="text/javascript">
+	$(function () {
+		
+		var filterList = {
+		
+			init: function () {
+			
+				// MixItUp plugin
+				// http://mixitup.io
+				$('#portfoliolist').mixitup({
+					targetSelector: '.portfolio',
+					filterSelector: '.filter',
+					effects: ['fade'],
+					easing: 'snap',
+					// call the hover effect
+					onMixEnd: filterList.hoverEffect()
+				});				
+			
+			},
+			
+			hoverEffect: function () {
+			
+				// Simple parallax effect
+				$('#portfoliolist .portfolio').hover(
+					function () {
+						$(this).find('.label').stop().animate({bottom: 0}, 200, 'easeOutQuad');
+						$(this).find('img').stop().animate({top: -30}, 500, 'easeOutQuad');				
+					},
+					function () {
+						$(this).find('.label').stop().animate({bottom: -40}, 200, 'easeInQuad');
+						$(this).find('img').stop().animate({top: 0}, 300, 'easeOutQuad');								
+					}		
+				);				
+			
+			}
+
+		};
+		
+		// Run the show!
+		filterList.init();
+			
+	});	
+	</script>
+	<!-- Add fancyBox main JS and CSS files -->
+<link href="css/magnific-popup.css" rel="stylesheet" type="text/css">
+<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
+		<script>
+			$(document).ready(function() {
+				$('.popup-with-zoom-anim').magnificPopup({
+					type: 'inline',
+					fixedContentPos: false,
+					fixedBgPos: true,
+					overflowY: 'auto',
+					closeBtnInside: true,
+					preloader: false,
+					midClick: true,
+					removalDelay: 300,
+					mainClass: 'my-mfp-zoom-in'
+			});
+		});
+		</script>
 </head>
 <body>
 	<?php include_once('header.php');?>
@@ -32,12 +96,43 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <div class="main_bg">
 <div class="wrap">
 	<div class="main">
+             <section id="content">
 		<div class="content" >
-			
+                    <table border="1" style="margin: 0 auto; width:100%;">
+                        <tr>
+                            <td>
+                                
+                                    <!-- CODIGO PHP PARA RECUPERAR LAS CATEGORIAS -->
+									<?php 	
+										require_once 'config.php';
+										require_once 'conexion.php';
+	 
+										$conexion = conectar();
+										$categorias=mysql_query("select * from CATEGORIA") or
+										die("Problemas en el select:".mysql_error());
+										
+										echo "<select name='select1' id='select1' >"; 
+										echo "<option>"'Seleccione'"</option>"; 
+										while ($row=mysql_fetch_array($categorias))){ 
+											echo "<option value='$row[0]'>" $row[0] "</option>"; 
+										}
+										echo "</select>"; 
+										
+										?>
+                                
+                            </td>
+                            <td>                                
+                            </td>
+                            <td>                                
+                            </td>
+                        </tr>
+                    </table>                    
 		</div>
+             </section>
 	</div>
 </div>
 </div>
 	<?php include_once('footer.php');?>
+
 </body>
 </html>
